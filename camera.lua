8f3ade56171a51b5835500133b1ee4a8f43fe52b
Yimo <heh1@live.fr>
2018-07-09 00:24:11 +0100

    Oops, forgot to update camera

--- camera.lua
+++ camera.lua
@@ -2,15 +2,21 @@
 -- GLOBAL (width) and (height) VARIABLES ARE SET UP IN THE LOVE.LOAD FUNCTION (CONTAINING THE CAMERA COORDINATES)
 -- INCASE OF SCREEN SIZE RECHANGE, (camera.needs_update) NEEDS TO CHANGE TO TRUE.
 local camera = {}
+
+camera.w, camera.h = love.graphics.getDimensions();
+camera.diagonal_squared = (camera.w^2 + camera.h^2)
+camera.diagonal = camera.diagonal_squared^0.5
+
 camera.x = 0;
 camera.y = 0;
-camera.scale = 1;
+camera.scalex = 1;
+camera.scaley = 1;
 camera.shearx = 0;
 camera.sheary = 0;
 camera.angle = 0;
 camera.ox, camera.oy = -width/2, -height/2;
 
-camera.transform = love.math.newTransform(camera.x, camera.y, camera.angle,camera.scale, camera.scale,camera.ox, camera.oy, camera.shearx, camera.sheary);
+camera.transform = love.math.newTransform(camera.x, camera.y, camera.angle,camera.scalex, camera.scaley, camera.ox, camera.oy, camera.shearx, camera.sheary);
 
 camera.needs_update = false;
 
@@ -21,7 +27,6 @@ end
 
 function camera:unset()
 	love.graphics.pop();
-	--love.graphics.applyTransform(self.inv_transform);
 end
 
 function camera:translate(dx, dy)
@@ -40,11 +45,24 @@ function camera:get_position()
 	return camera.x, camera.y;
 end
 
+function camera:set_scale(sx, sy)
+	if not (camera.scalex == sx and camera.scaley == sy) then
+		camera.scalex = sx;
+		camera.scaley = sy;
+		camera.needs_update = true;
+	end
+end
+
+function camera:get_scale()
+	return camera.scalex, camera.scaley;
+end
+
 function camera:set_origin(x, y)
-	camera.ox = x;
-	camera.oy = y;
-	camera.needs_update = true;
-	camera.needs_update = true;
+	if not (camera.ox == x and camera.oy == y) then
+		camera.ox = x;
+		camera.oy = y;
+		camera.needs_update = true;
+	end
 end
 
 function camera:get_origin()
@@ -68,9 +86,18 @@ end
 function camera:update(dt)
 	-- Small fix commit
 	if camera.needs_update then
-		camera.transform = camera.transform:setTransformation(camera.x, camera.y, camera.angle, camera.scale, camera.scale, camera.ox, camera.oy, camera.shearx, camera.sheary);
+		camera.transform = camera.transform:setTransformation(0, 0, camera.angle, 1, 1, camera.ox, camera.oy, camera.shearx, camera.sheary);
+		camera.transform = camera.transform:scale(camera.scalex, camera.scaley):translate(camera.x, camera.y);
 		camera.needs_update = false;
 	end
+end
+
+function camera:get_world_coordinates(x, y)
+	return self.transform:inverseTransformPoint(x, y)
+end
+
+function camera:get_screen_coordinates(x, y)
+	return self.transform:transformPoint(x, y)
 end
 
 return camera;
